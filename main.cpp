ff7f11a2adfa50e90208da8da65a347400702c20
Khylko Andrii <hilko_a@itstep.academy>
2021-12-01 17:04:19 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "sum.h"
+#include "prod.h"
+#include "quot.h"
+#include "dif.h"
+
+using namespace std;
+
+int main()
+{
+		cout<<"Hello Git\nSum = "<<Sum(12,23)<<endl;
+		cout<<"Prod = "<<Prod(12,23)<<endl;
+		cout<<"Quot = "<<Quot(12,23)<<endl;
+		cout<<"Dif = "<<Dif(12,23)<<endl;
+}
